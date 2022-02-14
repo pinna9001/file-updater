101251dcfb00699dddb217704a3ebc1ad09cec14
pinna <pinna9001@web.de>
2022-02-14 22:25:29 +0100

    added prints for help added support for command line arguments and options

--- src/python/client/client.py
+++ src/python/client/client.py
@@ -2,6 +2,7 @@ import socket
 import hashlib
 import os
 import sys
+import re
 
 BUF_SIZE = 1024
 
@@ -10,6 +11,9 @@ INSTRUCTION_HASH = 1
 INSTRUCTION_FILE = 2
 
 RETURN_VALUE_FILE_DOESNT_EXIST = 8
+
+safemode = False
+updating = True
 
 def getHashOfFile(filename):
     if not os.path.exists(filename):
@@ -42,7 +46,7 @@ def requestFileHash(socket, filename):
         return recv
 
 def updateFile(socket, directoryPath, filepath):
-    print(filepath, ": \n\tFiles are not matching. Updating...")
+    print("\tUpdating...")
     dataToSend = bytearray()
     dataToSend.append(INSTRUCTION_FILE)
     dataToSend += (filepath.encode("ascii"))
@@ -58,40 +62,61 @@ def updateFile(socket, directoryPath, filepath):
 def checkFilePathForUpdatesAvailable(socket, directoryPath, filepath):
     fileHash = requestFileHash(socket, filepath)
     if fileHash == None:
-        print(filepath, ": \n\tFiles does not exist on server. Deleting...")
-        os.remove(filepath)
-        return False
+        if safemode:
+            print(filepath, " \n\tFile does not exist on server. Safemode prevented deletion...")
+            return False
+        else:
+            print(filepath, ": \n\tFile does not exist on server. Removing...")
+            os.remove(os.path.join(directoryPath, filepath))
+            return False
     elif fileHash == getHashOfFile(os.path.join(directoryPath, filepath)):
         print(filepath, ": \n\tFiles are matching.")
         return False
     else:
+        print(filepath, ": \n\tFiles are not matching.")
         return True
 
-def checkForUpdates(directoryPath):
-    if not os.path.exists(directoryPath):
-        print(directoryPath, ": \n\tPath does not exist. Specify a existing path.")
-        exit()
-
+def checkForUpdates(directoryPath, address, port):
     relativePathToAllFiles = [os.path.relpath(os.path.join(r, file), directoryPath) for r, s, f in os.walk(directoryPath) for file in f]
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("localhost", 12345))
+    s.connect((address, port))
 
     for filepath in relativePathToAllFiles:
         filepath = filepath.replace(os.path.sep, "/")
         if checkFilePathForUpdatesAvailable(s, directoryPath, filepath):
-            updateFile(s, directoryPath, filepath)
-
+            if updating:
+                updateFile(s, directoryPath, filepath)
+            else:
+                print("\tUpdate prevented. Do not use the \"-c\" option to update the file.")
     s.close()
 
 def printHelp():
-    print("Help")
+    print("Usage: client.py [-h] [-s] <sourceaddress>:<port> <directory>\n")
+    print("Options:")
+    print(" -h: \tPrint help.")
+    print(" -s: \tSafemode. Files that are not present on the server will not be deleted.")
+    print(" -c: \tCheck only. Shows changed files, but prevents updates.")
+
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
+    if len(sys.argv) < 3:
         printHelp()
         exit()
-    if sys.argv[1] == "-h":
+    for arg in sys.argv[1:]:
+        if arg == "-h":
+            printHelp()
+            exit()
+        elif arg == "-s":
+            safemode = True
+        elif arg == "-c":
+            updating = False
+    if not re.match(".+:[0-9]+", sys.argv[-2]).span()[1] == len(sys.argv[-2]):
         printHelp()
-    else:
-        checkForUpdates(sys.argv[1])
+        exit()
+    if not os.path.exists(sys.argv[-1]):
+        print(sys.argv[-1], ": \n\tPath does not exist. Specify a existing path.\n")
+        printHelp()
+        exit()
+    connectiondata = sys.argv[-2].split(":")
+    checkForUpdates(sys.argv[-1], connectiondata[0], int(connectiondata[1]))
