94c58c8d629ccddb7028dae1a7b4b464f3b1c0f4
pinna <pinna9001@web.de>
2022-02-14 12:25:10 +0100

    update server and client

--- src/python/client/client.py
+++ src/python/client/client.py
@@ -1,5 +1,7 @@
 import socket
 import hashlib
+import os
+import sys
 
 BUF_SIZE = 1024
 
@@ -7,7 +9,13 @@ INSTRUCTION_CLOSE = 0
 INSTRUCTION_HASH = 1
 INSTRUCTION_FILE = 2
 
+RETURN_VALUE_FILE_DOESNT_EXIST = 8
+
+filename = "file3.txt"
+
 def getHashOfFile(filename):
+    if not os.path.exists(filename):
+        return None
     md5 = hashlib.md5()
     with open(filename, 'rb') as f:
         while True:
@@ -17,35 +25,51 @@ def getHashOfFile(filename):
             md5.update(data)
     return md5.digest()
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.connect(("raspberrypi", 12345))
+# returns the requested Filehash or None,
+# if the file doesnt exist on the server
+def requestFileHash(socket, filename):
+    dataToSend = bytearray()
+    dataToSend.append(INSTRUCTION_HASH)
+    dataToSend += (filename.encode("ascii"))
+    socket.send(dataToSend)
 
+    recv = socket.recv(1024)
+    if not recv:
+        print("closed connection")
+        exit()
+    elif int.from_bytes(recv, byteorder='big') == RETURN_VALUE_FILE_DOESNT_EXIST:
+        return None
+    else:
+        return recv
 
-dataToSend = bytearray()
-dataToSend.append(INSTRUCTION_HASH)
-dataToSend += ("file.txt".encode("ascii"))
-s.send(dataToSend)
+def main(directoryPath):
+    print("main")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("localhost", 12345))
+    fileHash = requestFileHash(s, filename)
 
-data = s.recv(1024)
+    if fileHash == None:
+        print("file doesn't exist on server. File will be deleted.")
+        os.remove("F:/file-updater/src/python/client/"+filename)
+    elif fileHash == getHashOfFile("F:/file-updater/src/python/client/"+filename):
+        print("Current files are matching. Nothing will be updated.")
+    else:
+        print("Current files are not matching. The existing file will be updated.")
 
-if not data:
-    print("closed Connection.")
-    exit()
-if data == getHashOfFile("G:\\updater\\client\\file.txt"):
-    print("current files are matching. Nothing will be updated.")
-else: 
-    print("Current files are not matching. The current file will be updated.")
+        dataToSend = bytearray()
+        dataToSend.append(INSTRUCTION_FILE)
+        dataToSend += ("file3.txt".encode("ascii"))
+        s.send(dataToSend)
 
-dataToSend = bytearray()
-dataToSend.append(INSTRUCTION_FILE)
-dataToSend += ("file.txt".encode("ascii"))
-s.send(dataToSend)
+        data = s.recv(1024)
 
-data = s.recv(1024)
+        fo = open("F:/file-updater/src/python/client/"+filename, "wb")
 
-fo = open("G:\\updater\\client\\file.txt", "wb")
+        fo.write(data)
 
-fo.write(data)
+    s.close()
 
-s.close()
+if __name__ == "__main__":
+    print(sys.argv)
+    main(".")
--- src/python/server/server.py
+++ src/python/server/server.py
@@ -2,12 +2,15 @@ import socket
 import select
 from time import sleep
 import hashlib
+import sys
+import os
 
-
-# arguments: SOURCE_TYPE=<local> REMOTE_SOURCE=<None> BUFFSIZE=<1024> HASH_ALGORITHM=<md5> 
+# arguments: SOURCE_TYPE=<local> REMOTE_SOURCE=<None> BUFFSIZE=<1024> HASH_ALGORITHM=<md5>
 # ? VERSIONING=<boolean>, PRECOMPUTE_HASHES=<boolean>
 
 BUF_SIZE = 1024
+
+TIME_OUT = 60   # in seconds
 
 RECV = 0
 SEND = 1
@@ -16,15 +19,18 @@ INSTRUCTION_CLOSE = 0
 INSTRUCTION_HASH = 1
 INSTRUCTION_FILE = 2
 
+RETURN_VALUE_FILE_DOESNT_EXIST = 8
+
 def getFileAsByteArray(filename):
-    fo = open("G:\\updater\\server\\"+filename, "rb")
+    fo = open("./source/"+filename, "rb")
     return fo.read()
 
 def getHashOfFile(filename):
     md5 = hashlib.md5()
-    with open("G:\\updater\\server\\"+filename, 'rb') as f:
+    with open("./source/"+filename, 'rb') as f:
         while True:
             data=f.read(BUF_SIZE)
+            print(data)
             if not data:
                 break
             md5.update(data)
@@ -37,24 +43,27 @@ class Connection:
         self.state = RECV
         self.data = None
         self.instruction = None
-    
+
     def send(self):
         if self.state == RECV:
             return
         m_bytes = bytearray()
         if self.instruction == INSTRUCTION_HASH:
-            m_bytes += getHashOfFile(self.data)
+            if os.path.exists("./source/"+self.data):
+                m_bytes += getHashOfFile(self.data)
+            else:
+                m_bytes.append(RETURN_VALUE_FILE_DOESNT_EXIST)
         elif self.instruction == INSTRUCTION_FILE:
              m_bytes += getFileAsByteArray(self.data)
-        socket.send(m_bytes)
+        self.socket.send(m_bytes)
         self.state = RECV
-        
+
     def recv(self):
         if self.state != RECV:
             return
-        data = socket.recv(1024)
+        data = self.socket.recv(1024)
         if not data or data[0] == INSTRUCTION_CLOSE:
-            socket.close()
+            self.socket.close()
             return True
         self.data = data[1:].decode('ascii')
         if data[0] == INSTRUCTION_FILE or data[0] == INSTRUCTION_HASH:
@@ -64,26 +73,33 @@ class Connection:
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-serversocket.bind(("localhost", 12345))
+serversocket.bind((socket.gethostname(), 12345))
 serversocket.listen(5)
 
 sockets = [serversocket]
 socketConnection = dict()
 
-while True:
-    rdyRead, rdyWrite, inErr = select.select(sockets, sockets, [])
-    
-    for socket in rdyRead: 
-        if socket==serversocket:
-            (clientSocket, address) = serversocket.accept()
-            sockets.append(clientSocket)
-            socketConnection[clientSocket] = Connection(clientSocket)
-        else:
-            if socketConnection[socket].recv():
-                sockets.remove(socket)
-            
-    for socket in rdyWrite: 
-        if socket==serversocket:
-            pass
-        else:
-            socketConnection[socket].send()
+
+
+def main():
+    while True:
+        rdyRead, rdyWrite, inErr = select.select(sockets, sockets, [], TIME_OUT)
+
+        for socket in rdyRead:
+            if socket==serversocket:
+                (clientSocket, address) = serversocket.accept()
+                sockets.append(clientSocket)
+                socketConnection[clientSocket] = Connection(clientSocket)
+            else:
+                if socketConnection[socket].recv():
+                    sockets.remove(socket)
+
+        for socket in rdyWrite:
+            if socket==serversocket:
+                pass
+            else:
+                socketConnection[socket].send()
+
+if __name__ == "__main__":
+    print(sys.argv)
+    main()
